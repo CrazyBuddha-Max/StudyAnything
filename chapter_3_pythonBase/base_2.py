b9a0d35b5c0f2ba0fd6dbae748b94cac435d522a
CrazyBuddha-Max <1127413498@qq.com>
2026-07-15 10:13:03 +0800

    updated: game

---
+++ chapter_3_pythonBase/base_2.py
@@ -0,0 +1,11 @@
+str = "sdsffaffsocxi123344357645"
+print(str)
+print(str[2:7])
+print(str[0:-2])
+print(str[1:8:2])
+print(str[::-1])
+num1 = 123
+num1_2 = bin(num1)
+print(num1_2)
+num1_16 = hex(num1)
+print(num1_16)
---
+++ chapter_3_pythonBase/base_3.py
@@ -0,0 +1,2 @@
+for i in range(5):
+    print(f"这是第{i}次循环")
